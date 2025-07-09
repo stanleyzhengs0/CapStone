c971d305884cce2face4417f983adc5864ec9ce3
Harsh Patel <patelharshharsh1@gmail.com>
2025-07-09 13:26:12 -0400

    feat: implemented home page

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import "../App"
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to the Homepage</h1>
+    </div>
+  )
+}
+
+export default Home
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import Home from './components/Home.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/app" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
