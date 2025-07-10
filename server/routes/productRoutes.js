63449f4045f0af20718bec14bcc8572db8b14f93
Stanley Zheng <stanleyzhengs@gmail.com>
2025-07-10 14:29:54 -0400

    feat: Product Details API

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,13 +1,15 @@
 import express from 'express'
-import { getPaginatedProducts, getProducts } from '../controllers/productController.js'
+import { getPaginatedProducts, getProductDetail, getProducts } from '../controllers/productController.js'
 
 const router = express.Router()
 
-router.get('/', getProducts)
+// router.get('/', getProducts)
 
 // Supports both:
 // /api/products/page/2?limit=5
 // /api/products?page=2&limit=5
-router.get('/page/:page', getPaginatedProducts)
+router.get('/page', getPaginatedProducts)
+
+router.get('/product', getProductDetail)
 
 export default router
