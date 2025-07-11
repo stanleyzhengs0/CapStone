d5ce3e12d6ff12c3713541103576f29fc363b813
Stanley Zheng <stanleyzhengs@gmail.com>
2025-07-11 10:59:05 -0400

    feat: UserContext Implementation

---
+++ client/src/context/UserContext.jsx
@@ -0,0 +1,15 @@
+import { createContext, useContext, useState } from "react";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null); // You can hydrate from localStorage later
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => useContext(UserContext);
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,15 +1,19 @@
 import express from 'express'
 import { getPaginatedProducts, getProductDetail, getProducts } from '../controllers/productController.js'
+import { getProductsByCategory } from '../controllers/categoryController.js'
 
 const router = express.Router()
 
 // router.get('/', getProducts)
 
-// Supports both:
-// /api/products/page/2?limit=5
-// /api/products?page=2&limit=5
+
+// /api/products/page?page=2
 router.get('/page', getPaginatedProducts)
 
+
+// /api/products/product?idp=686ebe61330880df3240c9e7
 router.get('/product', getProductDetail)
+
+router.get('/', getProductsByCategory)
 
 export default router
