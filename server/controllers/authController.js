22b470e1a7ae9698f341e8c146798cc78938d2b0
Stanley Zheng <stanleyzhengs@gmail.com>
2025-07-11 09:56:34 -0400

    feat: Auth API LoginUser

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -41,7 +41,46 @@ export const registerUser = async (req, res) => {
     }
 }
 
-
 export const loginUser = async (req, res) => {
-    
-}
+    const { email, password } = req.body;
+
+    // 1. Check if all fields are entered
+    if (!email || !password) {
+        return res.status(400).json({ message: "Email and password required" });
+    }
+
+    try {
+        // 2. Find if user exsits
+        const user = await userCollection.findOne({ "email" :email });
+
+        if (!user) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        // 3. Compare entered password with stored hashed password
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        // // 4. Generate JWT token (optional)
+        // const token = jwt.sign(
+        //     { userId: user._id, email: user.email },
+        //     'your_jwt_secret', // Use env var in real apps
+        //     { expiresIn: '1h' }
+        // );
+
+        res.status(200).json({
+            message: "Login successful",
+            user: {
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email
+            }
+        });
+
+    } catch (error) {
+        return res.status(500).json({ message: "Login error", error });
+    }
+};
